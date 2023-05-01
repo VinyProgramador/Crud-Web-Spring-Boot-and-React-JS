2c89be5adfc952dbc06b9c484f90f407555e7ae0
VinyProgramador <vinicius.nunes@sptech.school>
2023-05-01 17:02:10 -0300

    Rotas

--- crud-application/src/App.js
+++ crud-application/src/App.js
@@ -1,8 +1,21 @@
+import Post from './pages/Post';
+import Feed from './pages/Feed';
+import NotFound from './pages/NotFound';
+import EditPost from './pages/EditPost';
+
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>Init application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Feed />}/>
+          <Route path='/post' element={<Post />}/>
+          <Route path='/edit' element={<EditPost />}/>
+          <Route path='/*' element={<NotFound />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
